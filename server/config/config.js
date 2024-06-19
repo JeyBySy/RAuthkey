60e5545204cf95ca4b542e2521aeda418b74a4a8
JeyBySy <njcocosa@gmail.com>
2024-06-20 02:03:06 +0800

    Initial commit

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  "development": {
+    "username": "oauth",
+    "password": "781b747c2f3be38bf2b83034",
+    "database": "oauth",
+    "host": "172.31.0.2",
+    "dialect": 'postgres',
+  },
+  // "test": {
+  //   "username": "root",
+  //   "password": "781b747c2f3be38bf2b83034",
+  //   "database": "oauth",
+  //   "host": "172.30.0.2",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "172.30.0.2",
+  //   "dialect": "mysql"
+  // }
+}
