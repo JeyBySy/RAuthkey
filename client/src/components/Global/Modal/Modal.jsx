6e50eb96e5c69951ef8940879f020a95bb7423fe
JeyBySy <njcocosa@gmail.com>
2024-11-14 01:08:06 +0800

    Fix positioning in modal

--- client/src/components/Global/Modal/Modal.jsx
+++ client/src/components/Global/Modal/Modal.jsx
@@ -10,12 +10,12 @@ const Modal = ({ isOpen, onClose, children, modalTitle }) => {
         }
     };
     return (
-        <div className="backdrop-blur-sm fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 text-white cursor-pointer " onClick={handleOutsideClick}>
+        <div className="backdrop-blur-sm fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 text-white cursor-pointer z-20" onClick={handleOutsideClick}>
             <motion.div
                 initial={{ scale: 0.92 }}
                 animate={{ scale: 1 }}
                 transition={{ duration: 0.3, ease: "easeOut" }}
-                className="drop-shadow-2xl z-2 bg-elephant-900 rounded-lg shadow-lg w-[95%] sm:max-w-[500px] lg:w-[40%] lg:max-h-[50vh]  lg:absolute lg:top-[10%] overflow-hidden flex flex-col" onClick={(e) => e.stopPropagation()}>
+                className="drop-shadow-2xl z-2 bg-elephant-900 rounded-lg shadow-lg w-[95%] sm:max-w-[500px] lg:w-[40%]  lg:absolute lg:top-[10%] overflow-hidden flex flex-col" onClick={(e) => e.stopPropagation()}>
                 <div className="flex p-6">
                     <h2 className="text-xl font-semibold w-[96%]">{modalTitle}</h2>
                     <button className=" absolute right-5 w-6 h-6 bg-gray-500 rounded-full flex items-center justify-center hover:bg-slate-400 text-sm transition text-gray-100" onClick={handleOutsideClick}>&#10005;</button>
