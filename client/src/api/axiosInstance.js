bd222e588b6276017fcf52d41a416f30a3041dfc
JeyBySy <njcocosa@gmail.com>
2024-11-14 01:01:49 +0800

    remove comments

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -26,11 +26,8 @@ axiosInstance.interceptors.request.use(async (config) => {
     // If CSRF token is not set, attempt to fetch it and store in sessionStorage
     if (!csrfToken) {
         try {
-            // const response = await axios.get(`${BASE_URL}/api/csrf-token`);
-            // const csrfTokenFromServer = response.data.csrfToken;
             const csrfTokenFromServer = config.headers.csrfmiddlewaretoken
             sessionStorage.setItem('csrfToken', csrfTokenFromServer);
-            // config.headers['csrfmiddlewaretoken'] = csrfTokenFromServer;
         } catch (error) {
             console.error('Error fetching CSRF token:', error);
         }
