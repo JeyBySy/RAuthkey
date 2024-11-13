872cdfa8692d86c7ab40d7746bbe7ac66ef74f91
JeyBySy <njcocosa@gmail.com>
2024-11-14 01:03:19 +0800

    Change className to single class in css

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,47 @@
 // import Main from './Main';
 
+import DashboardCard from "../../components/Dashboard/DashboardCard"
+
 const Dashboard = () => {
     return (
-        <div className='lg:w-full container mx-auto'>
+        <div className='lg:w-full mx-auto'>
             <div className="flex mb-5">
                 <div className='w-full leading-8'>
-                    <h1 className="text-4xl">Dashboard</h1>
+                    <h1 className="title_page">Dashboard</h1>
                     <span className='text-gray-500'>Create new application for authentication</span>
                 </div>
+            </div>
+            <div className="flex flex-wrap flex-row justify-evenly items-center w-full border">
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+                <DashboardCard />
+
             </div>
         </div>
 
--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -3,7 +3,7 @@ const Users = () => {
         <div className='lg:w-full container mx-auto'>
             <div className="flex mb-5">
                 <div className='w-full leading-8'>
-                    <h1 className="text-4xl">Users</h1>
+                    <h1 className="title_page">Users</h1>
                     <span className='text-gray-500'>Create new application for authentication</span>
                 </div>
             </div>
