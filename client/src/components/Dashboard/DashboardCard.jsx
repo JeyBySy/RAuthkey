7086c2b672290f7ffcf7a2fab69ea60d6d3ad021
JeyBySy <njcocosa@gmail.com>
2024-11-19 22:37:26 +0800

    Make template for dashboard data

--- client/src/components/Dashboard/DashboardCard.jsx
+++ client/src/components/Dashboard/DashboardCard.jsx
@@ -1,7 +1,12 @@
-const DashboardCard = () => {
+// eslint-disable-next-line react/prop-types
+const DashboardCard = ({ children, cardTitle, className }) => {
     return (
-        <div className="w-[200px] border h-[200px]">DashboardCard</div>
-    )
-}
+        <div className={`flex flex-col shadow-elephant-700 shadow-md rounded-md p-4 w-fit ${className}`}>
+            <h3 className="text-lg font-semibold mb-2">{cardTitle}</h3>
+            <div>{children}</div>
+        </div>
+    );
+};
+
 
 export default DashboardCard
